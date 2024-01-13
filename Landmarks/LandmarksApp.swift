a97a4b2bce31b6d331364ddefe2acbdfbfae4d31
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-01-13 01:27:16 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mauricio Fontana on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
