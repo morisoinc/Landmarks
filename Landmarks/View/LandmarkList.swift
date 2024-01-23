7a3e3a10a4704fdddd9d5a5e88c88e430055fdf0
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-01-22 21:06:51 -0300

    added favorites filter, changed landmarks to be observable

--- Landmarks/View/LandmarkList.swift
+++ Landmarks/View/LandmarkList.swift
@@ -7,19 +7,31 @@
 
 import SwiftUI
 
-import SwiftUI
-
-
 struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            !showFavoritesOnly || landmark.isFavorite
+        }
+    }
+
     var body: some View {
         NavigationSplitView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")
@@ -27,7 +39,7 @@ struct LandmarkList: View {
     }
 }
 
-
 #Preview {
     LandmarkList()
+        .environment(ModelData())
 }
