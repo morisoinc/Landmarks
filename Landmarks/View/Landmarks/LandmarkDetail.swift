875bfd8f67301cdc2d7a6594c4f7cedad7d62222
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-01-22 21:19:23 -0300

    allowing the user to add/remove faves

--- Landmarks/View/Landmarks/LandmarkDetail.swift
+++ Landmarks/View/Landmarks/LandmarkDetail.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
 
     var body: some View {
+        @Bindable var modelData = modelData
+
         ScrollView {
             VStack {
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 MapView(coordinate: landmark.locationCoordinate)
                     .frame(height: 300)
             
@@ -49,5 +61,7 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[0])
+    let modelData = ModelData()
+    return LandmarkDetail(landmark: modelData.landmarks[0])
+        .environment(modelData)
 }
